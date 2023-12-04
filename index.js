5844e9925d70ea8eedbef8b7717af4c69f7ef905
NaveenReddy0427 <naveendeva909@gmail.com>
2023-12-04 14:24:33 +0530

    create main router and controller

--- index.js
+++ index.js
@@ -2,6 +2,12 @@ const express = require('express');
 const app = express();
 const port = 3500;
 
+
+// setup router middleware
+app.use('/', require('./routes/index'));
+
+
+
 app.listen(port, (req, res)=>{
     console.log(`server listening on port ${port}`);
 })
