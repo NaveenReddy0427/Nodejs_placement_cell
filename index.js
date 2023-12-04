8117e10a7566dc1c4e62b29dd7f18a65b80dc1ec
NaveenReddy0427 <naveendeva909@gmail.com>
2023-12-04 16:36:07 +0530

    created layout for the two ejs files

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const app = express();
 const port = 3500;
+
+// setup the static file to use app
+app.use(express.static('./assets'))
+
 // setup the layouts
 const expressLayouts = require('express-ejs-layouts');
 app.use(expressLayouts);
+
 
 // setup router middleware
 app.use('/', require('./routes/index'));
