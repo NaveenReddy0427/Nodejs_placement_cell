2a7258dd7ab85d510ec0202d2985d346ba3674dc
NaveenReddy0427 <naveendeva909@gmail.com>
2023-12-04 16:03:35 +0530

    created layout

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
 const port = 3500;
+// setup the layouts
+const expressLayouts = require('express-ejs-layouts');
+app.use(expressLayouts);
 
 // setup router middleware
 app.use('/', require('./routes/index'));
